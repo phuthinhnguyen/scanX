016242554a1e93877b63c9f669965bfecfcdccdb
phuthinhnguyen <phuthinhnguyen1101@gmail.com>
2024-10-30 21:25:55 +0700

    sdf

--- src/components/Loadingpath.js
+++ src/components/Loadingpath.js
@@ -3,7 +3,7 @@ function Loadingpath() {
     return (
         <div className="preloader-wrap">
             <div className="header">
-                <h2 className="logotext">scannow</h2>
+                <h2 className="logotext">scanX</h2>
             </div>
             <div className="preloader">
                 <img src="/img/Preloader_7.gif" alt=""></img>
